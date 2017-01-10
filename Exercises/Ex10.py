3893e81d634db5f6cad94ad46beed28e64ccb4f7
ngramstad <ngramstad@gmail.com>
2017-01-10 09:36:16 -0800

    Testing a message

---
+++ Exercises/Ex10.py
@@ -0,0 +1,27 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+# Testing single quote escape
+test_cat = '''
+My test list:
+\t* Go!
+\t* Vandals!
+What?
+Is?
+This?
+'''
+
+print test_cat
---
+++ Exercises/Ex13_Study.py
@@ -0,0 +1,10 @@
+#Study questions for Ex13
+from sys import argv
+
+team, color = argv
+
+team = raw_input("What's your favorite team?")
+color = raw_input("What's your favorite color?")
+
+print "What is your favorite team?" (team)
+print "What is your favorite color?" (color)
